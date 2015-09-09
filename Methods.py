910b3d734f9015e40f26e06bda30ebe7fac371a8
Brennan-S <brennan.stein@hotmail.com>
2015-09-09 14:50:16 -0700

    Made better, prettier.

--- Methods.py
+++ Methods.py
@@ -1,33 +1,41 @@
 print ("It's over " + "9000")
 print ("It's over " + "the rainbow")
 print ("It's over " + "between us")
-
+#--------------------#
 repeat = "It's over"
 print (repeat + "9000")
 print (repeat + "the rainbow")
 print (repeat + "between us")
-
+#--------------------#
 def itsOver(whatsOver):
     print ("It's over " + whatsOver)
 
 itsOver("9000")
 itsOver("the rainbow")
 itsOver("between us")
+#--------------------#
+def makeTwentyPercentCooler(percentage):
+    return "it's " + str(percentage * 1.2) + "% cool"
 
-def twentyPercentCooler(percentage):
-    return "It's " + str(percentage * 1.2) + "% cool"
+print "If you take 100% cool and make it twenty percent cooler," + makeTwentyPercentCooler(100)
+#--------------------#
+def basicMath(num1, operator, num2):
+    if operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+    elif operator == "*":
+        return num1 * num2
+    elif operator == "/":
+        return num1 / num2
+    else:
+        return "Error: Invalid operator"
 
-print ("You know, " + twentyPercentCooler("413"))
-
-def aYoungInTheir(x,y):
-    return "A young " + x + " stands in their " + y
-print (aYoungInTheir("villan","fortress of evil"))
-
-def johnnyCash(sue):
-    print ("Now, I don't blame him cause he run and hid")
-    print ("But the meanest thing that he ever did")
-    print ("Was before he left, he went and named me " + sue)
-johnnyCash("Cthulhu")
-
-lineOfText = input("This is the prompt")
-#Write program that prints out the song that never ends n times, where n is inputted through the
+print "The answer to 3 + 4 is" + str(basicMath(3, "+", 4)
+print "The answer to 3 - 4 is" + str(basicMath(3, "-", 4)
+print "The answer to 3 * 4 is" + str(basicMath(3, "*", 4)
+print "The answer to 3 / 4 is" + str(basicMath(3, "/", 4)
+print "The answer to 3 # 4 is" + str(basicMath(3, "#", 4)
+#--------------------#
+name = input("Greetings, USER. What is your name?")
+print "Hello, " + name + "."
