4ab816c2809da00dea7bb851519d98ad926225fe
Brennan-S <brennan.stein@hotmail.com>
2015-09-09 14:31:33 -0700

    Made better, prettier.

--- Numbers.py
+++ Numbers.py
@@ -1,10 +1,11 @@
-print (1)
-print (3)
-print (1.5)
-print (1+1)
-print (1 * 1.5)
-print (1/4)
-print (1.0/4.0)
-print (1 + 4 * 3)
-print ((1 + 4) * 3)
-#Make python do a problem from your maths homework
+number = 13
+print (number)
+#--------------------#
+number = 0
+#--------------------#
+print (123.456)
+#--------------------#
+print (3 * 6 + 4)
+print (3 * (6 + 4))
+#--------------------#
+print (4 * number)
