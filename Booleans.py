5352fc6b2266734b1e468f3776da2c390b4e6451
Brennan-S <brennan.stein@hotmail.com>
2015-09-09 14:15:52 -0700

    Made better, prettier.

--- Booleans.py
+++ Booleans.py
@@ -1,10 +1,21 @@
 boolean = True
 print (boolean)
+#--------------------#
 print (413 == 413)
 print ("apples" == "apples")
-print (13 == 4)
+print (1 == 2)
 print ("apples" == "gorram oranges")
-print (4 < 13)
-print (13 > 4)
-print (11 > 11)
-print (11>= 11)
+#--------------------#
+print (1 == 2)
+print (1 != 2)
+print (1 < 2)
+print (1 > 2)
+print (1 <= 2)
+print (1 >= 2)
+#--------------------#
+print (1 == 1)
+print (1 != 1)
+print (1 < 1)
+print (1 > 1)
+print (1 <= 1)
+print (1 >= 1)
