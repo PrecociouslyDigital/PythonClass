aa8f6b13933f5542841681e5fb0e1e9f65fa3b10
Brennan-S <brennan.stein@hotmail.com>
2015-09-09 14:37:56 -0700

    Made better, prettier.

--- Casting.py
+++ Casting.py
@@ -1,6 +1,13 @@
 print ("You can " + "add strings together")
-print ("But you can't add a string to a number like " + 413)
-print ("To turn a number into a string, use " + str(413))
-#to turn a string into a number, use
-int("413")
-float("4.13")
+print ("But you can't add a string to a number like")
+print ("'string' + 123")
+print ("That's because numbers aren't strings!")
+#--------------------#
+print ("To turn a number into a string, use ")
+print ("str(413)")
+print (str(413) + " is a number!")
+#--------------------#
+print ("You can also do the opposite, turning a string into a number!")
+integer = int("413")
+decimal = float("4.13")
+print (str(integer) + "   " + str(decimal))
