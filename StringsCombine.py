291ad235998d4d20311d8bdde3a54b9af9c43d98
Brennan-S <brennan.stein@hotmail.com>
2015-09-09 14:27:56 -0700

    Made better, prettier.

--- StringsCombine.py
+++ StringsCombine.py
@@ -1,2 +1,9 @@
-print ("Don't stop" + "Beliveingggggggggggggggggggggggggggggg")
-#Nothing else works though
+string1 = "I want to combine this string"
+string2 = "with this one"
+
+print string1 + string2
+#--------------------#
+print "Squishing" + "Strings" + "Together"
+#--------------------#
+string1 = "Old string"
+print string1 + " and New String"
