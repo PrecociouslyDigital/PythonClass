c54ffd35b8916062d832ba3479b6532f9f312ee0
Brennan-S <brennan.stein@hotmail.com>
2015-09-09 14:22:55 -0700

    Made better, prettier.

--- Loops.py
+++ Loops.py
@@ -1,12 +1,32 @@
 boolean = True
 while boolean:
     print ("this loops while the boolean value is true. Given that that boolean doesn't seem to be changing anytime soon, we're going to be stuck here for a while")
-counter = 1
-while counter < 6:
+#--------------------#
+counter = 0
+while counter <= 10:
     print (counter)
-
-list = [1,2,3,4,5]
+#--------------------#
+counter = 0
+while counter < 10:
+    print (counter)
+    counter = counter + 1
+    
+counter = 0
+while counter <= 10:
+    print (counter)
+    counter = counter + 1
+#--------------------#
+counter = 0
+while counter <= 24:
+    print (counter)
+    counter += 2
+#--------------------#
+list = [1, 2, 8, 4, "Hi"]
 for x in list:
     print (x)
-for x in range(1,5):
+#--------------------#
+for x in range(1, 5):
+    print (x)
+    
+for x in range(1, 10):
     print (x)
